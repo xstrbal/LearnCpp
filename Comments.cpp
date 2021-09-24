00a03da75c277d24ab667e574f0f7c50dac8f1e1
Lukas <xstrbal@stuba.sk>
2021-09-24 13:33:06 +0200

    Comments

---
+++ Comments.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Hello world!\n"; // Everything from here to the end of the line is ignored
+
+	std::cout << "Hello world!\n"; // std::cout lives in the iostream library
+	std::cout << "It is very nice to meet you!\n"; // these comments make the code hard to read
+	std::cout << "Yeah!\n"; // especially when lines are different lengths
+
+	return 0;
+}
