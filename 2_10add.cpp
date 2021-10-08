8bdb4d078981f4cf3a3f4cb5aefbc6aa1e36fa5b
Lukas <xstrbal@stuba.sk>
2021-10-08 17:08:44 +0200

    Header Files

---
+++ 2_10add.cpp
@@ -0,0 +1,6 @@
+#include "2_10add.h"
+
+int add(int x, int y)
+{
+	return x + y;
+}
---
+++ 2_10main.cpp
@@ -0,0 +1,9 @@
+#include "2_10add.h"
+#include <iostream>
+
+int main()
+{
+	std::cout << "The sum of 3 and 4 is " << add(3, 4) << '\n';
+	return 0;
+}
+
